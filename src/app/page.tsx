7ac51e2a87b4ae11f5e86cdfcdfa354a39d0f492
Joni Niemelä <j.niemela@outlook.com>
2025-08-13 17:28:01 +0300

    Updated README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
-import React, { useState , useContext} from "react";
-import { gql, useQuery, useMutation } from "@apollo/client";
+import React from "react";
+import { useQuery } from "@apollo/client";
 import { GET_CURRENTLY_RUNNING_TRAINS } from '@/graphql/queries';
 import {TrainQueryItem} from "@/utils/types";
 
 export default function Home() {
 
   // Fetch all Trains
-  const { loading, error, data, refetch } = useQuery(GET_CURRENTLY_RUNNING_TRAINS);
+  const { loading, error, data} = useQuery(GET_CURRENTLY_RUNNING_TRAINS);
   console.log(data)
 
   return (
       <div>
         <h1>VR Trains</h1>
-        {loading ? <div>Loading...</div> : <ul>
+        {loading ? <div>Loading...</div> : error ? <div>Error while gathering data!</div> : <ul>
           {data.currentlyRunningTrains.map((train: TrainQueryItem) => (
             <div key={train.trainNumber + train.departureDate}>
               <div>Junan numero: {train.trainNumber}</div>
