637bf6990baca118fb700856955753d0eed70ce3
Joni <j.niemela@outlook.com>
2025-08-16 22:48:24 +0300

    New fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/graphql/:path*',
-        destination: 'https://rata.digitraffic.fi/api/v2/graphql/:path*',
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
